3a3f20ccadf86631037b854e8f039318fb81727f
Tsering Lama <tseringlama@pursuit.org>
2020-03-02 23:54:40 -0500

    added few cocoa pods

---
+++ FireAuthLab/FireBase Services/AuthSession.swift
@@ -0,0 +1,32 @@
+//
+//  AuthSession.swift
+//  FireAuthLab
+//
+//  Created by Tsering Lama on 3/2/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthenticationSession {
+    public func createNewUser(email: String, password: String, completion: @escaping (Result<AuthDataResult, Error>) -> ()) {
+        Auth.auth().createUser(withEmail: email, password: password) { (authDataResult, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let authDataResult = authDataResult {
+                completion(.success(authDataResult))
+            }
+        }
+    }
+    
+    public func signExistingUser(email: String, password: String, completion: @escaping (Result<AuthDataResult, Error>) -> ()) {
+        Auth.auth().signIn(withEmail: email, password: password) { (authDataResult, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let authDataResult = authDataResult {
+                completion(.success(authDataResult))
+            }
+        }
+    }
+}
---
+++ FireAuthLab/ViewControllers/LoginVC.swift
@@ -0,0 +1,71 @@
+//
+//  LoginVC.swift
+//  FireAuthLab
+//
+//  Created by Tsering Lama on 3/2/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+
+
+import UIKit
+import Comets
+import Gradients
+
+class LoginVC: UIViewController {
+    
+    let backgroundLayer = Gradients.amourAmour.layer
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.layer.addSublayer(backgroundLayer)
+        startComets()
+    }
+    
+    private func startComets() {
+        
+        // Customize your comet
+        let width = view.bounds.width
+        let height = view.bounds.height
+        let comets = [Comet(startPoint: CGPoint(x: 100, y: 0),
+                            endPoint: CGPoint(x: 0, y: 100),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.black),
+                      Comet(startPoint: CGPoint(x: 0.4 * width, y: 0),
+                            endPoint: CGPoint(x: width, y: 0.8 * width),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.black),
+                      Comet(startPoint: CGPoint(x: 0.8 * width, y: 0),
+                            endPoint: CGPoint(x: width, y: 0.2 * width),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.black),
+                      Comet(startPoint: CGPoint(x: width, y: 0.2 * height),
+                            endPoint: CGPoint(x: 0, y: 0.25 * height),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.black),
+                      Comet(startPoint: CGPoint(x: 0, y: height - 0.8 * width),
+                            endPoint: CGPoint(x: 0.6 * width, y: height),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.black),
+                      Comet(startPoint: CGPoint(x: width - 100, y: height),
+                            endPoint: CGPoint(x: width, y: height - 100),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.black),
+                      Comet(startPoint: CGPoint(x: 0, y: 0.8 * height),
+                            endPoint: CGPoint(x: width, y: 0.75 * height),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.black)]
+        
+        // draw track and animate
+        for comet in comets {
+            view.layer.addSublayer(comet.drawLine())
+            view.layer.addSublayer(comet.animate())
+        }
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        backgroundLayer.frame = view.bounds
+    }
+    
+}
