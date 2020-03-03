9c2e6ede577e45e9ad74f09b048eead694139412
Tsering Lama <tseringlama@pursuit.org>
2020-03-02 22:51:44 -0500

    Initial Commit

---
+++ FireAuthLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireAuthLab
+//
+//  Created by Tsering Lama on 3/2/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
