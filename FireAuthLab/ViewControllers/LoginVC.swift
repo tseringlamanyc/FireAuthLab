a65134c1574ac8e0cc5e8e6f33aad4f72b575142
Tsering Lama <tseringlama@pursuit.org>
2020-03-03 00:43:54 -0500

    set up the UI for the login vc

--- FireAuthLab/ViewControllers/LoginVC.swift
+++ FireAuthLab/ViewControllers/LoginVC.swift
@@ -11,13 +11,24 @@ import UIKit
 import Comets
 import Gradients
 
+enum AccountState {
+    case existingUser
+    case newUser
+}
+
 class LoginVC: UIViewController {
+    
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
     
     let backgroundLayer = Gradients.amourAmour.layer
     
+    private var authSession = AuthenticationSession()
+    
+    private var accountState: AccountState = .existingUser
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.layer.addSublayer(backgroundLayer)
         startComets()
     }
     
@@ -53,6 +64,34 @@ class LoginVC: UIViewController {
                       Comet(startPoint: CGPoint(x: 0, y: 0.8 * height),
                             endPoint: CGPoint(x: width, y: 0.75 * height),
                             lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.black),
+                      Comet(startPoint: CGPoint(x: 0, y: 100),
+                            endPoint: CGPoint(x: 100, y: 0),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.black),
+                      Comet(startPoint: CGPoint(x: 0, y: 0.4 * width),
+                            endPoint: CGPoint(x: width, y: 0.8 * width),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.black),
+                      Comet(startPoint: CGPoint(x: 0, y: 0.8 * width),
+                            endPoint: CGPoint(x: width, y: 0.2 * width),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.black),
+                      Comet(startPoint: CGPoint(x: 0.2 * height, y: width),
+                            endPoint: CGPoint(x: 0, y: 0.25 * height),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.black),
+                      Comet(startPoint: CGPoint(x: 0, y: height - 0.8 * width),
+                            endPoint: CGPoint(x: 0.6 * width, y: height),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.black),
+                      Comet(startPoint: CGPoint(x: width - 100, y: height),
+                            endPoint: CGPoint(x: width, y: height - 100),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.black),
+                      Comet(startPoint: CGPoint(x: 0, y: 0.8 * height),
+                            endPoint: CGPoint(x: width, y: 0.75 * height),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
                             cometColor: UIColor.black)]
         
         // draw track and animate
@@ -66,6 +105,14 @@ class LoginVC: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         backgroundLayer.frame = view.bounds
+    }
+    
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func signUpButton(_ sender: UIButton) {
     }
     
 }
